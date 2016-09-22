f191a45d050c5daa9eb7f4e73704f4bd7fb88d89
Sam Learner <learnersd@gmail.com>
2016-09-22 13:21:20 -0400

    Small fixes made to tracker_results.py

--- tracker_results.py
+++ tracker_results.py
@@ -14,45 +14,45 @@ import sys
 cats = ['locations', 'technologies']
 groups = ['Java/.NET', 'Top Camp', 'Selected Camp', 'Potential Markets', 'Current Markets']
 
-if len(sys.argv) < 2:
-	print 'USAGE: python tracker_results.py days_back (OPTIONAL:) [tracking_group1] ... [tracking_groupx]'
-	sys.exit()
+def main():
+	if len(sys.argv) < 2:
+		print 'USAGE: python tracker_results.py days_back (OPTIONAL:) [tracking_group1] ... [tracking_groupx]'
+		sys.exit()
 
-days_back = int(sys.argv[1])
+	days_back = int(sys.argv[1])
 
-tgroups = []
-for x in range(2, len(sys.argv)):
-	tgroups.append(sys.argv[x])
+	tgroups = []
+	for x in range(2, len(sys.argv)):
+		tgroups.append(sys.argv[x])
 
-for i, item in enumerate(tgroups):
-	tgroups[i] = return_closest(item, groups)
+	for i, item in enumerate(tgroups):
+		if item == 'ALL':
+			continue
+		tgroups[i] = return_closest(item, groups)
 
-if sys.argv[2] and sys.argv[2] == 'ALL':
-	tgroups = groups
+	if sys.argv[2] and sys.argv[2] == 'ALL':
+		tgroups = groups
 
-"""for c in cats:
-	for g in groups:
-		print c
-		print g
-		print tracked_camp_changes(14, c, g)
-		print"""
 
-tracking_group_stats(days_back)
-
-print
-
-for c in cats:
-	print c.upper()
-	print tracked_camp_changes(days_back, c)
-	print
-
-for group in tgroups:
-	print group.upper().center(35, '-')
-	tracking_group_stats(days_back, group)
+	tracking_group_stats(days_back)
 
 	print
 
 	for c in cats:
 		print c.upper()
-		print tracked_camp_changes(days_back, c, group)
+		print tracked_camp_changes(days_back, c)
 		print
+
+	for group in tgroups:
+		print group.upper().center(35, '-')
+		tracking_group_stats(days_back, group)
+
+		print
+
+		for c in cats:
+			print c.upper()
+			print tracked_camp_changes(days_back, c, group)
+			print
+
+if __name__ == '__main__':
+  main()
