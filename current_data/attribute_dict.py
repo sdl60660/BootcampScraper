f347a0358799df7e26985f12507ebf9dc33e86e1
Sam Learner <learnersd@gmail.com>
2016-09-30 17:16:32 -0400

    Added an attribute dictionary (attribute_dict.py) to current_data to be loaded by a wrapper function and used to translate search terms into JSON categories and then back into the search terms for display purposes (also realized that for a detailed display in a bootcamp search, not all categories need to be displayed when it's from the slackbot (i.e. cr_technologies and su_technologies wouldn't be helpful to anyone if they searched a bootcamp.

---
+++ current_data/attribute_dict.py
@@ -0,0 +1,11 @@
+Attribute_Dict = {	
+	'cr_num_reviews': "Course Report Reviews"
+	'cr_rating': "Course Report Rating"
+	'job_assistance': "Job Assistance"
+	'job_guarantee': "Job Guarantee"
+	'num_courses': "Number of Courses"
+	'num_locations': "Number of Locations"
+	'tracking_groups': "Tracking Groups"
+	'su_rating': "SwitchUp Rating"
+	'su_num_reviews': "Swithcup Reviews"
+}
