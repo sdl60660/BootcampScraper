1a825ab3ea28223b8aabf7359e8d0a6a38d92a92
Sam Learner <learnersd@gmail.com>
2016-09-30 17:44:56 -0400

    Added an __init__.py file to current_data so that attribute_dict coiuld be called. Started the term translation process on search_wrapper.py, but it needs to be finished still.

--- current_data/attribute_dict.py
+++ current_data/attribute_dict.py
@@ -1,11 +1,17 @@
-Attribute_Dict = {	
-	'cr_num_reviews': "Course Report Reviews"
-	'cr_rating': "Course Report Rating"
-	'job_assistance': "Job Assistance"
-	'job_guarantee': "Job Guarantee"
-	'num_courses': "Number of Courses"
-	'num_locations': "Number of Locations"
-	'tracking_groups': "Tracking Groups"
-	'su_rating': "SwitchUp Rating"
-	'su_num_reviews': "Swithcup Reviews"
+
+Out_Dict = {	
+	'cr_num_reviews': "Course Report Reviews",
+	'cr_rating': "Course Report Rating",
+	'job_assistance': "Job Assistance",
+	'job_guarantee': "Job Guarantee",
+	'num_courses': "Number of Courses",
+	'num_locations': "Number of Locations",
+	'tracking_groups': "Tracking Groups",
+	'su_rating': "SwitchUp Rating",
+	'su_num_reviews': "Swithcup Reviews",
+	'last_updated': "Last Updated",
+}
+
+In_Dict = {	
+	v: k for k,v in Out_Dict.iterms()
 }
